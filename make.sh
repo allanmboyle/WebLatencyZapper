c06a4a37b56e5f6c08195707d17d6fb0f19ab24c
Bradley Smith <bradley.smith@myob.com>
2012-07-18 09:44:18 +1000

    Changes to automate the azure deployment

--- make.sh
+++ make.sh
@@ -2,8 +2,8 @@
 
 mkdir build
 cp -R deployments/AzureDeployment build
-cp -R public 		build/AzureDeployment/WebLatencyZapperWebRole
-cp -R node_modules 	build/AzureDeployment/WebLatencyZapperWebRole
-cp -R *.js 			build/AzureDeployment/WebLatencyZapperWebRole
+cp -R public 		build/AzureDeployment/WebRole1
+cp -R node_modules 	build/AzureDeployment/WebRole1
+cp -R *.js 			build/AzureDeployment/WebRole1
 
 echo Build complete
